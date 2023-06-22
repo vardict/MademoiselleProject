3e4e5a98104b8c499cad17ae5c2add683e6bda22
vardict <tijanavardic012@gmail.com>
2023-06-22 12:53:13 +0200

    Contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -41,5 +41,61 @@ $(document).ready(function () {
             }
         });
     }
+    
+        //Form validation
+    
+    if ($('.contact-form').length > 0) {
+        $('.contact-form').validate({
+
+            //sa ovim mozemo da hajlajtujemo neku gresku koju korisnik napravu. U obliku funkcije je gde pisemo sta trereba dase desi prilikom greske. Funkciija prima element koji se validira
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+
+            //ovo je objekat gde za svako polje definisemo sta treba da se desi
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true
+                },
+                subject:{
+                    required: true 
+                }
+            },
+            messages: {
+                name: {
+                    required: 'The Name and surname * field is required'
+                },
+                email: {
+                    required: 'The Email address * field is required',
+                    email: 'Pleas provide a valid email address'
+                },
+                message: {
+                    required: 'The Message * field is required'
+                },
+                subject: {
+                    required: 'The Subject * field is required'
+                }
+            },
+
+            //element u kome zelimo da pisemo nase greske
+            errorElement: 'p',
+            //ova funkcija prima dva paramenta a to su error i element
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest('.form-group').find('.error-msg'));
+            }
+
+        });
+    }
 });
 
